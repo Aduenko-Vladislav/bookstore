df2a2bd5ea36500ace7db075d4e7be568b4be6b2
Vladislav Aduenko <adyenko@gmail.com>
2022-05-19 01:06:08 +0300

    feat(app): add books on home page

---
+++ src/components/Title/style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { typography } from "../../ui/typography";
+
+export const StyledTitle = styled.h1`
+  ${typography.H1};
+
+  margin-bottom: 48px;
+`;
