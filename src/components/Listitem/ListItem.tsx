71179e0e679af68c260d3509fb7793216c4053ec
Vladislav <adyenko@gmail.com>
2022-05-23 00:52:40 +0300

    feat(bookdetails): add new page

--- src/components/Listitem/ListItem.tsx
+++ src/components/Listitem/ListItem.tsx
@@ -1,5 +1,12 @@
 import { INewBookApi } from "../../services/types";
-import { BookPrice, BookSubtitle, BookTitle, StyledImg, StyledListItem } from "./styles";
+import {
+  BookPrice,
+  BookSubtitle,
+  BookTitle,
+  StyledImg,
+  StyledLink,
+  StyledListItem,
+} from "./styles";
 
 interface IListItem {
   book: INewBookApi;
@@ -8,12 +15,14 @@ interface IListItem {
 const ListItem = ({ book }: IListItem) => {
   return (
     <StyledListItem>
-      <StyledImg src={book.image} alt={book.title} />
-      <BookTitle>{book.title}</BookTitle>
-      <BookSubtitle>{book.subtitle}</BookSubtitle>
+      <StyledLink to={`/bookstore/books/${book.isbn13}`}>
+        <StyledImg src={book.image} alt={book.title} />
+        <BookTitle>{book.title}</BookTitle>
+        <BookSubtitle>{book.subtitle}</BookSubtitle>
         <BookPrice>
           {book.price === "$0.00" ? "Free for you" : book.price}
         </BookPrice>
+      </StyledLink>
     </StyledListItem>
   );
 };
