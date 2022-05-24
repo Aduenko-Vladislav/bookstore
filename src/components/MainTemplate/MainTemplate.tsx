d4a26e79a329c73ebe045e46d6f7bcb5301d9ffe
Vladislav Aduenko <adyenko@gmail.com>
2022-05-25 01:27:33 +0300

    feat(main): add styles

--- src/components/MainTemplate/MainTemplate.tsx
+++ src/components/MainTemplate/MainTemplate.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
-import { Container } from "./styles";
+import { Container, StyledApp } from "./styles";
 
 const MainTemplate = () => {
   return (
-    <Container>
-      <Header />
-      <Outlet />
-      <Footer />
-    </Container>
+    <StyledApp>
+      <Container>
+        <Header />
+        <Outlet />
+        <Footer />
+      </Container>
+    </StyledApp>
   );
 };
 
