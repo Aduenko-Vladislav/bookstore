c74d367d46b71ef76269bbc2485cd722dccd90f9
Vladislav Aduenko <adyenko@gmail.com>
2022-06-07 00:16:16 +0300

    feat(loading): add loading

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,16 @@
+import { LeoBorderOne, LeoBorderTwo, LeoCoreOne, LeoCoreTwo, SpinnerBox } from "./styles";
+
+export const Loading = () => {
+  return (
+    <SpinnerBox>
+      <LeoBorderOne>
+        <LeoCoreOne />
+      </LeoBorderOne>
+      <LeoBorderTwo>
+        <LeoCoreTwo />
+      </LeoBorderTwo>
+    </SpinnerBox>
+  );
+};
+
+
