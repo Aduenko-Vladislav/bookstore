9642a7afe38ff40fb0816eca6c7c7c14997828e5
Vladislav Aduenko <adyenko@gmail.com>
2022-06-08 01:11:04 +0300

    feat(account): add new page

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,12 +1,26 @@
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
+import BackButton from "../components/BackButton/BackButton";
+import { Profile } from "../components/Profile/Profile";
+import { Title } from "../components/Title/Title";
 import { routes } from "../routes/routes";
 import { RootState } from "../store/store";
 
 export const Account = () => {
   const { isAuth } = useSelector(({ user }: RootState) => user);
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
   if (isAuth) {
-    return <div>Account</div>;
+    return (
+      <>
+        <BackButton onClick={handleBack} />
+        <Title>Account</Title>
+        <Profile />
+      </>
+    );
   }
   return <Navigate to={routes.SIGN_IN} />;
 };
